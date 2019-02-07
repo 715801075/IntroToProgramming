3e11d4c645a630068e401229d9d1d3fe917850a4
Holiday Course <holiday@ad.aie.edu>
2019-02-06 21:02:44 -0800

    initial

---
+++ Assets/IntroToProgamming-master/Assets/_Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    
+    public GameObject Owner;
+    public float DamageValue = 1;
+
+   public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != Owner)
+        {
+            health tempHealth = other.GetComponent<health>(
+          );
+            if (tempHealth != null)
+            {
+                tempHealth.TakeDamage(DamageValue);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public float MaxHealth;
+    public float CurrentHealth;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    public void Death()
+
+    {
+        Destroy(gameObject);
+
+    }
+
+    public void TakeDamage(float DamageAmount)
+    {
+        CurrentHealth = MaxHealth;
+        CurrentHealth -= DamageAmount;
+
+        if (CurrentHealth <= 0)
+        { Death();
+        }
+    }
+    // Update is called once per frame
+    
+
+
+}
+
